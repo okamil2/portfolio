f8d698b7ede2593972efefb7c2d7fe3b580ed809
okamil <okamil2@uwo.ca>
2023-08-20 19:27:40 -0400

    backing up all the files here

---
+++ javascript/scripts.js
@@ -0,0 +1,85 @@
+// $(document).ready(function () {
+
+//     $("#sidebar").mCustomScrollbar({
+//          theme: "minimal"
+//     });
+
+//     $('#sidebarCollapse').on('click', function () {
+//         // open or close navbar
+//         $('#sidebar').toggleClass('active');
+//         // close dropdowns
+//         $('.collapse.in').toggleClass('in');
+//         // and also adjust aria-expanded attributes we use for the open/closed arrows
+//         // in our CSS
+//         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+//     });
+
+// });
+/**
+ * Easy selector helper function
+ */
+// var select = (el, all = false) => {
+//     el = el.trim()
+//     if (all) {
+//       return [...document.querySelectorAll(el)]
+//     } else {
+//       return document.querySelector(el)
+//     }
+//   }
+
+// var typed = select('.typed')
+// if (typed) {
+//   let typed_strings = typed.getAttribute('data-typed-items')
+//   typed_strings = typed_strings.split(',')
+//   new Typed('.typed', {
+//     strings: typed_strings,
+//     loop: true,
+//     typeSpeed: 100,
+//     backSpeed: 50,
+//     backDelay: 2000
+//   });
+// }
+
+
+document.addEventListener('DOMContentLoaded',function(event){
+    // array with texts to type in typewriter
+    var dataText = [ "a Developer.", "UX/UI Designer.", "IT Specialist.", "Always Learning."];
+    
+    // type one text in the typwriter
+    // keeps calling itself until the text is finished
+    function typeWriter(text, i, fnCallback) {
+      // chekc if text isn't finished yet
+      if (i < (text.length)) {
+        // add next character to h1
+       document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+  
+        // wait for a while and call this function again for next character
+        setTimeout(function() {
+          typeWriter(text, i + 1, fnCallback)
+        }, 100);
+      }
+      // text finished, call callback if there is a callback function
+      else if (typeof fnCallback == 'function') {
+        // call callback after timeout
+        setTimeout(fnCallback, 700);
+      }
+    }
+    // start a typewriter animation for a text in the dataText array
+     function StartTextAnimation(i) {
+       if (typeof dataText[i] == 'undefined'){
+          setTimeout(function() {
+            StartTextAnimation(0);
+          }, 20000);
+       }
+       // check if dataText[i] exists
+      if (i < dataText[i].length) {
+        // text exists! start typewriter animation
+       typeWriter(dataText[i], 0, function(){
+         // after callback (and whole text has been animated), start next text
+         StartTextAnimation(i + 1);
+       });
+      }
+    }
+    // start the text animation
+    StartTextAnimation(0);
+  });
